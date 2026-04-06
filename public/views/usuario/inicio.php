e6a950c12e615f75d17521b8468d0526f5f9a62b
LaurenSof <laurenoviedo68@gmail.com>
2026-04-05 23:17:14 -0500

    barra

--- public/views/usuario/inicio.php
+++ public/views/usuario/inicio.php
@@ -196,9 +196,10 @@ try {
 
         .topbar {
             position: absolute;
-            top: 8px;
-            left: 0;
-            right: 0;
+             top: 8px;
+             left: 50%;
+            transform: translateX(-50%);
+            width: 70%;
             z-index: 3001;
             height: 56px;
             padding: 0 14px;
